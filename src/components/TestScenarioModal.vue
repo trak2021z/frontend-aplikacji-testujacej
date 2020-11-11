7b72146a7263f721b6c4f02d3df5ce5b819f2c2d
Kamil <kamil.kaczor1234@gmial.com>
2020-11-11 12:30:46 +0100

    Test scenario tree works with backend

--- src/components/TestScenarioModal.vue
+++ src/components/TestScenarioModal.vue
@@ -1,23 +1,31 @@
 <template>
-  <div class="modal fade" id="modalTestScenario" tabindex="-1" role="dialog" aria-labelledby="buySellStockModal"
-       aria-hidden="true" data-keyboard="false" data-backdrop="static">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title w-100 font-weight-bold">Test Scenario Actions</h4>
-          <button @click="hide" type="button" class="close position-absolute" style="right:4%" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        {{getTestScenario}}
-        <div class="modal-body mx-3">
-          <test-tree-item
-              class="item"
-              :item="treeData"
-          ></test-tree-item>
+  <div>
+    <template v-if="this.getTestScenario === null">
+    </template>
+
+    <template v-else>
+      <div class="modal fade" id="modalTestScenario" tabindex="-1" role="dialog" aria-labelledby="buySellStockModal"
+           aria-hidden="true" data-keyboard="false" data-backdrop="static">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title w-100 font-weight-bold">Test Scenario Actions</h4>
+              <button @click="hide" type="button" class="close position-absolute" style="right:4%" data-dismiss="modal"
+                      aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body mx-3">
+              <test-tree-item
+                  v-for="(branch) in treeData" :key="branch.value.Id"
+                  class="item"
+                  :item="branch"
+              ></test-tree-item>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -36,31 +44,122 @@ export default {
   },
   data() {
     return {
+      isComputing: null,
+      treeData: null,
+      arrayOfEndpointsIds: null,
+      /*
       treeData: {
         name: "Register",
         children: [
-          { name: "Log in" },
+          {name: "Log in"},
           {
             name: "Buy Action",
             children: [
               {
                 name: "Sell Action",
-                children: [{ name: "Log out" }, { name: "Log in" }]
+                children: [{name: "Log out"}, {name: "Log in"}]
               },
-              { name: "Create Offer" },
-              { name: "Cancel Offer" },
+              {name: "Create Offer"},
+              {name: "Cancel Offer"},
               {
                 name: "Create Offer",
-                children: [{ name: "Check if Offer Changed Status" }, { name: "Log out" }]
+                children: [{name: "Check if Offer Changed Status"}, {name: "Log out"}]
               }
             ]
           }
         ]
       }
+     */
     }
   },
   methods: {
     ...mapActions(["getTestScenarioById"]),
+    buildHierarchy(arry) {
+
+      var roots = [], children = {};
+
+      // find the top level nodes and hash the children based on parent
+      for (var i = 0, len = arry.length; i < len; ++i) {
+        var item = arry[i],
+            p = item.Parent,
+            target = !p ? roots : (children[p] || (children[p] = []));
+
+        target.push({value: item});
+      }
+
+      // function to recursively build the tree
+      var findChildren = function (parent) {
+        if (children[parent.value.Id]) {
+          parent.children = children[parent.value.Id];
+          for (var i = 0, len = parent.children.length; i < len; ++i) {
+            findChildren(parent.children[i]);
+          }
+        }
+      };
+
+      // enumerate through to handle the case where there are multiple roots
+      for (i = 0, len = roots.length; i < len; ++i) {
+        findChildren(roots[i]);
+      }
+
+      return roots;
+    },
+    buildTreeData() {
+      let arrayOfEndpoints = this.getTestScenario.endpoints.map(function (e) {
+        if (e.order === 0) {
+          return {"Id": e.id.toString(), "Name": e.name, "Parent": ""}
+        } else {
+          return {"Id": e.id.toString(), "Name": e.name, "Parent": e.order}
+        }
+      });
+
+      console.log(arrayOfEndpoints);
+
+      for (let i = 0; i < arrayOfEndpoints.length; i++) {
+        let id = 9999;
+        if (arrayOfEndpoints[i].Parent.length > 1) {
+          for (let j = arrayOfEndpoints[i].Parent.length - 1; j > 0; j--) {
+            let removedId = arrayOfEndpoints[i].Parent.splice(-1,1);
+            let object = {"Id": (id--).toString(), "Name": arrayOfEndpoints[i].Name, "Parent": [removedId]}
+            arrayOfEndpoints.push(object);
+          }
+        }
+      }
+
+      arrayOfEndpoints = arrayOfEndpoints.map(function (e) {
+          return {"Id": e.Id, "Name": e.Name, "Parent": e.Parent.toString()}
+      });
+
+      console.log(arrayOfEndpoints);
+      let arrayOfEndpointsIds = [];
+
+      arrayOfEndpointsIds = arrayOfEndpoints.map(function (e) {
+            return e.Id;
+          }
+      );
+
+      arrayOfEndpointsIds = arrayOfEndpointsIds.filter((v, i, a) => a.indexOf(v) === i);
+      arrayOfEndpointsIds = arrayOfEndpointsIds.sort();
+
+      this.arrayOfEndpointsIds = arrayOfEndpointsIds;
+
+      for (let i = 0; i < this.arrayOfEndpointsIds.length; i++) {
+        for (let j = 0; j < this.arrayOfEndpointsIds.length; j++) {
+          if (arrayOfEndpoints[j].Id === this.arrayOfEndpointsIds[i]) {
+            let parent = arrayOfEndpointsIds.indexOf(arrayOfEndpoints[j].Parent)
+            if (parent === -1) {
+              arrayOfEndpoints[j].Parent = "";
+            } else {
+              arrayOfEndpoints[j].Parent = (parent + 1).toString();
+            }
+            arrayOfEndpoints[j].Id = (i + 1).toString();
+          }
+        }
+      }
+
+      this.treeData = this.buildHierarchy(arrayOfEndpoints);
+      console.log(this.treeData);
+    },
     show() {
       jQuery('#modalTestScenario').modal()
     },
@@ -72,12 +171,13 @@ export default {
       this.isComputing = true;
       try {
         let response = await this.getTestScenarioById(this.testPk);
-        if(response.status !== 200){
+        if (response.status !== 200) {
           alert(`${response.status}: ${response.data.error}`);
         }
-      }catch(e){
+      } catch (e) {
         console.log(e);
       }
+      this.buildTreeData();
       this.isComputing = false;
     }
   },
@@ -91,9 +191,9 @@ export default {
     }
   },
   watch: {
-    isVisible: async function(newVal) {
-      if(newVal){
-        //await this.getNewScenario();
+    isVisible: async function (newVal) {
+      if (newVal) {
+        await this.getNewScenario();
         this.show();
       }
     }
