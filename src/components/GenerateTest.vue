dd79ad8a148922d762eed2943a13b7bc51877d0b
mzyga4 <37455997+mzyga4@users.noreply.github.com>
2020-11-23 09:40:14 +0100

    forcing modal close before redirect
    forcing modal close before redirect

--- src/components/GenerateTest.vue
+++ src/components/GenerateTest.vue
@@ -72,6 +72,7 @@ import {required, numeric, minValue, maxValue} from "vuelidate/lib/validators";
 import TestProgressModal from "@/components/TestProgressModal";
 import {mapGetters, mapActions} from "vuex";
 import router from "@/router";
+import jQuery from 'jquery';
 
 export default {
     name: "GenerateTests",
@@ -87,6 +88,7 @@ export default {
         edt_users: null,
         edt_queries: null,
         isComputing: false,
+        allowATRmessage: false,
         tests: {
           pk: "0",
           name: "testName",
@@ -112,8 +114,14 @@ export default {
               {
                 this.getDoneTests();
                 
-                if(this.allDoneTests.length == 0) this.isLastTestFinished = true;
-                this.isLastTestFinished = this.allDoneTests[0].is_finished;
+                if(this.allDoneTests.length == 0){
+                  this.isLastTestFinished = true;
+                }
+                else{
+                  this.allowATRmessage = true;
+                  this.isLastTestFinished = this.allDoneTests[0].is_finished;
+                }
+              
                 if(this.isLastTestFinished == null) this.isLastTestFinished = true;
 
               }).bind(this), 3000)
@@ -162,7 +170,10 @@ export default {
                         else 
                         { 
                           this.testCallId = id;
-
+                          this.closeModal();
+                          clearInterval(this.timerUT);
+                          this.$emit('hide');
+                          jQuery('#modalTestProgress').modal('hide');
                           router.replace('/test/done/' + this.testCallId)
 
                           if(doneTestResponse.data.is_finished)
@@ -171,6 +182,7 @@ export default {
 
                             this.isTestCallCompleted = true;
                           }
+
                         }
                       });
                     }).bind(this), 10000)
@@ -197,7 +209,7 @@ export default {
       clearInterval(this.timer);
       this.isTestProgressModalVisible = false;
       this.isTestCallCompleted = false;
-      this.testCallId = 0;
+      //this.testCallId = 0;
     }
   },
   async created() {
