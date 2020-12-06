70c4889d4903117f85cbf52f6b6e642185d45dd1
Kamil <kamil.kaczor1234@gmial.com>
2020-12-06 15:28:24 +0100

    max queries validation added to generate test form

--- src/components/GenerateTest.vue
+++ src/components/GenerateTest.vue
@@ -46,6 +46,7 @@
                 <span v-if="!$v.edt_queries.required">Test Queries are required</span>
                 <span v-else-if="!$v.edt_queries.minValue">Test Queries amount must be a positive number</span>
                 <span v-else-if="!$v.edt_queries.numeric">Test Queries amount must be a numeric value</span>
+              <span v-else-if="!$v.edt_queries.maxValue">Test Queries must not exceed {{maxRequests}}</span>
             </div>
           </div><br>
           <div class="row">
@@ -103,7 +104,8 @@ export default {
         isTestCallCompleted: false,
         testResultsSize: 0,
         testCallId: 0,
-        maxUsers: process.env.VUE_APP_TEST_MAX_USERS
+        maxUsers: process.env.VUE_APP_TEST_MAX_USERS,
+        maxRequests:  process.env.VUE_APP_TEST_MAX_REQUESTS
     }
   },
   methods: {
@@ -233,6 +235,7 @@ export default {
         required,
         numeric,
         minValue: minValue(1),
+        maxValue: maxValue(this.maxRequests)
       }
     }
   },
